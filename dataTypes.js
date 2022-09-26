98fda5ee2dceaf6e1575f3ecb680b2fe42737666
RachelCVerastique <95655014+RachelCVerastique@users.noreply.github.com>
2022-09-26 17:32:48 -0500

    simple refresh on basics

---
+++ dataTypes.js
@@ -0,0 +1,23 @@
+var example = 'This is a single backslash character: "\\"'
+var example2 = "Here's a number: \"two\""
+var modEx = 6 % 5
+
+
+
+var prob1 = 2 + 3 * 4;   // equals 14
+var prob2 = (2 + 3) * 4; // equals 20
+
+
+//String concatenation
+var myConCatString = 'Hello, ' + 'World!'
+var myConCatString2 = myConCatString + 'Its a good day to be ALIVE!'
+
+console.log(myConCatString2)
+console.log(myConCatString);
+console.log(modEx, prob1, prob2);
+console.log(example);
+console.log(example2);
+console.log(typeof(example));
+console.log(typeof '42');
+console.log(typeof true);
+
