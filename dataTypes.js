4873dc6cf3be3af16bf21c3b2cda8caf3b808b4a
RachelCVerastique <95655014+RachelCVerastique@users.noreply.github.com>
2022-09-26 21:40:48 -0500

    &&, ==, ||

--- dataTypes.js
+++ dataTypes.js
@@ -12,12 +12,69 @@ var prob2 = (2 + 3) * 4; // equals 20
 var myConCatString = 'Hello, ' + 'World!'
 var myConCatString2 = myConCatString + 'Its a good day to be ALIVE!'
 
-console.log(myConCatString2)
-console.log(myConCatString);
-console.log(modEx, prob1, prob2);
-console.log(example);
-console.log(example2);
-console.log(typeof(example));
-console.log(typeof '42');
-console.log(typeof true);
+var p1 = '1' + 2;
+
+var p2 = typeof '1' + 2;
+
+var p3 = 6 % 4
+
+var addMe = 3 + 4
+var p4 ='3 + 4 is ' + 3 + 4 + " OR 3 + 4 is " + addMe;
+// how could you make this produce the correct output?
+
+var p5 =0 < 0;
+
+var p6 = 'false' == false;
+
+var p7 = true == 'true';
+
+var p8 = 5 >= -5;
+
+var p9 = !false || false;
+
+var p10 = true || "42";
+
+var p11 = !true && !false;
+
+var p12 = 6 % 5;
+
+var p13 = 5 < 4 && 1 === 1;
+
+var p14 = typeof 'codeup' === 'string';
+
+var p15 = 'codeup' === 'codeup' && 'codeup' === 'Codeup';
+
+var p16 = 4 >= 0 && 1 !== '1';
+
+var p17 = 6 % 3 === 0;
+
+var p18 = 5 % 2 !== 0;
+
+
+console.log("1:" + p1);
+console.log("2:" + p2);
+console.log("3:" + p3);
+console.log("4:" + p4);
+console.log("5:" + p5);
+console.log("6:" + p6);
+console.log("7:" + p7);
+console.log("8:" + p8);
+console.log("9:" + p9);
+console.log("10:" + p10);
+console.log("11:" + p11);
+console.log("12:" + p12);
+console.log("13:" + p13);
+console.log("14:" + p14);
+console.log("15:" + p15);
+console.log("16:" + p16);
+console.log("17:" + p17);
+console.log("18:" + p18);
+// console.log(myConCatString2)
+// console.log(myConCatString);
+// console.log(modEx, prob1, prob2);
+// console.log(example);
+// console.log(example2);
+// console.log(typeof(example));
+// console.log(typeof '42');
+// console.log(typeof true);
 
